8b7ca6015340d3bcff13b87a364406ce4baf4e69
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2024-08-19 22:20:51 +0900

    [level 2] Title: 구명보트, Time: 21.77 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(people, limit):
+    cnt = 0
+    people = deque(sorted(people))
+    i, j = 0, len(people) - 1
+    
+    while i < j:
+        if people[i] + people[j] <= limit: 
+            i += 1
+        cnt += 1
+        j -= 1
+        if i == j: cnt += 1
+        
+    return cnt
