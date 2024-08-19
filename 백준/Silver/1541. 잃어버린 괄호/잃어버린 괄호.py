738a39fc65ac2d2d5833c768b625ce451f232c02
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2024-08-19 23:56:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 152 ms, Memory: 112896 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -2,22 +2,12 @@ import sys
 import re
 
 input = sys.stdin.readline
-exp = re.split(r'(\+|\-)', input().strip())
-minus, sum = 0, int(exp[0])
+exp = input().split('-')
+result = 0
 
-for i in range(1, len(exp), 2): # 홀수번만
-    next = int(exp[i + 1])
-    if exp[i] == '+':
-        if minus == 0:
-            sum += next
-        else:
-            minus += next
-    else:
-        if minus == 0:
-            minus += next
-        else:
-            sum -= minus
-            minus = next
-sum -= minus
+for idx, e in enumerate(exp):
+    e = list(map(int, e.split('+')))
+    if idx == 0: result += sum(e)
+    else: result -= sum(e)
 
-print(sum)
+print(result)
