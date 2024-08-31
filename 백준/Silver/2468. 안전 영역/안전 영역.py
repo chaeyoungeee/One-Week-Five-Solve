fcaa2bf4a2688fd5fda3d710139a4a844b565261
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2024-08-31 11:16:54 +0900

    [Silver I] Title: 안전 영역, Time: 424 ms, Memory: 130836 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -8,6 +8,11 @@ input = sys.stdin.readline
 n = int(input())
 graph = [ list(map(int, input().split())) for _ in range(n) ]
 
+max_h = 0
+
+for g in graph:
+    max_h = max(max_h, max(g))
+
 cnt = 0 # 안전 영역의 수
 max_cnt = 0 # 안전 영역 수의 최댓값
 
@@ -29,7 +34,7 @@ def bfs(i, j, k):
                 new_graph[nx][ny] = 0
                 queue.append((nx, ny))
 
-for k in range(1, 101):
+for k in range(1, max_h+1):
     new_graph = copy.deepcopy(graph) # 깊은 복사
     for i in range(n):
         for j in range(n):
