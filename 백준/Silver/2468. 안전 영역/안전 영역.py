9042b9e93c89c1320bce8f0eb49989b5d6620794
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2024-08-31 11:25:48 +0900

    [Silver I] Title: 안전 영역, Time: 364 ms, Memory: 127904 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -9,7 +9,6 @@ n = int(input())
 graph = [ list(map(int, input().split())) for _ in range(n) ]
 
 max_h = 0
-
 for g in graph:
     max_h = max(max_h, max(g))
 
@@ -20,8 +19,8 @@ dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
 def bfs(i, j, k):
-    new_graph[i][j] = 0
     queue = deque([(i, j)])
+    visited[i][j] = 1
 
     while queue:
         x, y = queue.popleft()
@@ -30,15 +29,15 @@ def bfs(i, j, k):
             ny = y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= n:
                 continue
-            if new_graph[nx][ny] >= k:
-                new_graph[nx][ny] = 0
+            if graph[nx][ny] >= k and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
                 queue.append((nx, ny))
 
 for k in range(1, max_h+1):
-    new_graph = copy.deepcopy(graph) # 깊은 복사
+    visited = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if new_graph[i][j] >= k:
+            if graph[i][j] >= k and visited[i][j] == 0:
                 bfs(i, j, k)
                 cnt += 1
     if max_cnt < cnt: max_cnt = cnt
