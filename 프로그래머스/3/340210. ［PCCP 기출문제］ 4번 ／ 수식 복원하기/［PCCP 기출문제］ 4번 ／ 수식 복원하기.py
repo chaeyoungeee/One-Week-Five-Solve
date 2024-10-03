235881732b1c7d05bc2363a7842600dc128e8eed
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2024-10-04 02:10:07 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 2.93 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.py
@@ -0,0 +1,86 @@
+from collections import deque
+
+def convert_to_decimal(b, n):
+    num = 0
+    for j in range(len(n)):
+        num += (b ** (len(n) - 1 - j)) * int(n[j])
+    return num
+
+def convert_form_decimal(b, n):
+    num = deque([])
+    n = int(n)
+    if n == 0:
+        return "0"
+    while b <= n: 
+        r = n % b
+        n //= b
+        num.appendleft(str(r))
+    num.appendleft(str(n))
+    return int(''.join(num))
+
+def calculate(a, b, operator):
+    if operator == '+': return a + b
+    else: return a - b
+
+def solution(expressions):
+    base = set()
+    m = 2
+    exp = []
+    exp2 = []
+    result = []
+    for e in expressions:
+        s = list(e.split(' '))
+        for i in s[0]:
+            m = max(m, int(i)+1)
+        for i in s[2]:
+            m = max(m, int(i)+1)
+    
+        if s[4] == 'X':
+            exp2.append(s)
+        else:
+            for i in s[4]:
+                m = max(m, int(i)+1)
+            exp.append(s)     
+
+    if m != 9:
+        for i in range(m, 10): base.add(i)
+        for e in exp:
+            for i in range(m, 10):
+                a = convert_to_decimal(i, e[0])
+                b = convert_to_decimal(i, e[2])
+                c = convert_to_decimal(i, e[4])
+                if calculate(a, b, e[1]) != c: 
+                    base.discard(i)
+    else:
+        base.add(m)
+        
+    if len(base) == 1:
+        ba = base.pop()
+        for e in exp2:
+            a = convert_to_decimal(ba, e[0])
+            b = convert_to_decimal(ba, e[2])
+            e[4] = str(convert_form_decimal(ba, calculate(a, b, e[1])))
+            result.append(' '.join(e))
+        return result
+    else:
+        for e in exp2:
+            v = -1
+            for ba in base:
+                a = convert_to_decimal(ba, e[0])
+                b = convert_to_decimal(ba, e[2])
+                c = str(convert_form_decimal(ba, calculate(a, b, e[1])))
+                print(ba,a,b,c)
+                if v == -1: v = c
+                else:
+                    if v != c:
+                        e[4] = '?'
+                        result.append(' '.join(e))
+                        break
+            else:
+                e[4] = v
+                result.append(' '.join(e))
+            
+    return result
+                        
+            
+
