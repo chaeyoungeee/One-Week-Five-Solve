8bcc2357215c2214af3acb6ccf793bf8832128a3
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2025-07-23 18:00:09 +0900

    [level 2] Title: 방문 길이, Time: 0.36 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -2,16 +2,14 @@ def solution(dirs):
     l = 0
     x, y = 5, 5
     d = {'U': (0,1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
-    dirs = list(dirs)
-    ways = set()
+    w = set()
     
     for dir in dirs:
         dx, dy = d[dir]
         nx, ny = x+dx, y+dy
         if 0 <= nx < 11 and 0 <= ny < 11:
-            if (x,y, nx, ny) not in ways:
-                l += 1
-            ways.add((x,y, nx, ny))
-            ways.add((nx, ny, x, y))
+            w.add((x,y, nx, ny))
+            w.add((nx, ny, x, y))
             x, y = nx, ny
-    return l
+            
+    return len(w) // 2
