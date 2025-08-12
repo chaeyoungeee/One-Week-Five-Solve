80e3d1cd2c5a9353db670cfbca37429bda9255bf
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2025-08-12 18:20:18 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.43 ms, Memory: 9.51 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from itertools import combinations
+
+def solution(orders, course):
+    result = []
+    for c in course:
+        menus = defaultdict(int)
+        for order in orders:
+            combs = list(combinations(order, c))
+            for comb in combs:
+                comb = sorted(comb)
+                menus["".join(comb)] += 1
+        menus = sorted(menus.items(), key=lambda x: -x[1])
+        
+        for menu in menus:
+            if menu[1] < 2: break
+            if menu[1] != menus[0][1]: break
+            result.append(menu[0])
+            
+    return sorted(result)
