95fec37d89e430d4a25770b1d9c0265a4e4ecc81
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2025-08-13 11:22:37 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 114.51 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,19 +1,13 @@
 def solution(enroll, referral, seller, amount):
     m = { e: 0 for e in enroll }
-    r = { e: r for e, r in zip(enroll, referral) }
+    r = dict(zip(enroll, referral))
     
     for s, a in zip(seller, amount):
         a = a * 100
-        while True:
-            a2 = a // 10
-            a1 = a - a2
-            m[s] += a
-            if a2 < 1: break
-            else:
-                m[s] -= a2
-                if r[s] == '-': break
-                a = a2
-                s = r[s]
+        while a > 0 and s != "-":
+            m[s] += (a - a // 10)
+            a //= 10
+            s = r[s]
     
-    return [ i[1] for i in m.items() ]
+    return [ m[e] for e in enroll ]
             
