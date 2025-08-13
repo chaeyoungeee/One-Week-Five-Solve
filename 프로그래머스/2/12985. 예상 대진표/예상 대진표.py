587b615dcb4e6dba595ce3c16cd937916b248900
Chaeyoung Lim <102286483+chaeyoungeee@users.noreply.github.com>
2025-08-13 10:14:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,14 +1,9 @@
-def next(k):
-    return (k + 1) // 2 if k % 2 else k // 2 
-
 def solution(n, a, b):
-    round = 1
+    round = 0
     
-    while True:
-        if (a % 2 and (a + 1) == b) or (not (a % 2) and (a - 1) == b):
-                return round
-        round += 1
-        a, b = next(a), next(b)
+    while a != b:
+        a, b = (a+1)//2, (b+1)//2
+        round+=1
         
-                
+    return round
                 
